192084b1efb62f4e4cd4e65750e7a439f244ddc7
Andrea Barchietto <andreabarchietto01@gmail.com>
2023-02-20 10:37:01 +0100

    add retry backoff to fetch

--- jaguar_uploader/src/index.js
+++ jaguar_uploader/src/index.js
@@ -3,24 +3,11 @@ import CryptoJs from 'crypto-js';
 
 //On page load
 (function () {
-    /**
-     * When user upload a file, it will be divided in chunks of 5MB and
-     * sent to server with address /api/upload/chunk/<videoId>
-     * The body of the request must be an array of bytes of the chunk
-     * The content-range header must be set to the range of the chunk
-     * The content-type must be application/octet-stream
-     * The request method must be PUT
-     * The first request must be /api/upload/init/<videoId>
-     * The last request must be /api/upload/commit/<videoId>
-     * A group of request (es 5 or 10) must be sent in parallel
-     * If Promise.all fails, the upload must be restarted
-     * On the first (init) and last (commit) request must be sent alone
-     * The first request contains a body json with data: {name: <fileName>, size: <fileSize>, container: <fileContainer>}
-     * The videoId is a UUID V4
-     * The fileContainer is the extension of the file
-     * The file name is the name of the file
-     * The file size is the size of the file expressed in bytes
-     */
+
+    function isFetchOk(code) {
+        return code >= 200 && code < 300;
+    }
+
     var uploads = []
     const initUpload = async (file, videoId) => {
         const response = await fetch(`http://192.168.178.137:3000/upload/begin/${videoId}`, {
@@ -34,16 +21,17 @@ import CryptoJs from 'crypto-js';
                 container: file.name.split('.').pop()
             })
         })
-        if (response.status>=200&&response.status<300) return true
+        if (response.status >= 200 && response.status < 300) return true
         return false
     }
     const commitUpload = async (file, videoId) => {
         const response = await fetch(`http://192.168.178.137:3000/upload/commit/${videoId}`, {
             method: 'POST'
         })
-        if (response.status>=200&&response.status<300) return true
+        if (response.status >= 200 && response.status < 300) return true
         return false
     }
+
     const uploadChunk = async (file, videoId) => {
         return new Promise((resolve, reject) => {
             const reader = new FileReader()
@@ -56,54 +44,59 @@ import CryptoJs from 'crypto-js';
                 const fileSize = file.size
                 const chunks = Math.ceil(fileSize / chunkSize)
                 for (let i = 0; i < chunks; i++) {
-                    if(end>fileSize) end = fileSize
+                    if (end > fileSize) end = fileSize
                     promises.push(
                         new Promise(async (resolve, reject) => {
-                            console.log(start, end, fileSize, reader.result.length)
-                            console.log(reader.result.slice(start, end))
                             const hash = CryptoJs.SHA256(CryptoJs.lib.WordArray.create(reader.result.slice(start, end))).toString(CryptoJs.enc.Hex)
-                            console.log(hash)
-                            //send binary array to server with a fetch request
-                            const response = await fetch(`http://192.168.178.137:3000/upload/chunk/${videoId}`, {
-                                method: 'POST',
-                                headers: {
-                                    'Content-Type': 'application/octet-stream',
-                                    'Content-Range': `bytes ${start}-${end - 1}/${fileSize}`,
-                                    'Content-Hash': hash
-                                },
-                                body: reader.result.slice(start, end)
-                            })
-                            if (response.status>= 200&&response.status<300) {
-                                resolve()
-                            } else {
-                                console.log(await response.text())
-                                reject()
+                            async function upload() {
+                                return new Promise(async (resolve, reject) => {
+                                    try {
+                                        const response = await fetch(`http://192.168.178.137:3000/upload/chunk/${videoId}`, {
+                                            method: 'POST',
+                                            headers: {
+                                                'Content-Type': 'application/octet-stream',
+                                                'Content-Range': `bytes ${start}-${end - 1}/${fileSize}`,
+                                                'Content-Hash': hash
+                                            },
+                                            body: reader.result.slice(start, end)
+                                        })
+                                        if (isFetchOk(response.status)) return resolve()
+                                        reject()
+                                    } catch (e) {
+                                        reject()
+                                    }
+                                })
                             }
+                            var backoff = 0
+                            while (backoff < 9) {
+                                try {
+                                    await upload()
+                                    return resolve()
+                                } catch (e) {
+                                    console.log('retrying...')
+                                    await new Promise(r => setTimeout(r, 1000 * Math.pow(2, backoff)))
+                                    backoff++
+                                }
+                            }
+                            reject()
                         })
                     )
                     start = end
                     end += chunkSize
                     if (promises.length < 10) continue
-                    while (1) {
-                        i++
-                        try {
-                            await Promise.all(promises)
-                            promises = []
-                            break
-                        } catch (e) {
-                            console.log('upload failed. Retrying...')
-                        }
-                    }
-                }
-                if(promises.length==0) return resolve()
-                while (1) {
                     try {
                         await Promise.all(promises)
                         promises = []
-                        break
                     } catch (e) {
-                        console.log('upload failed. Retrying...')
+                        return reject()
                     }
+                }
+                if (promises.length == 0) return resolve()
+                try {
+                    await Promise.all(promises)
+                    promises = []
+                } catch (e) {
+                    return reject()
                 }
                 resolve()
             }
